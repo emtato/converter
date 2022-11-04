b94e431f3996189c00f103b09334221618876b04
botato2048 <ema@tfs.ca>
2022-11-04 09:41:08 -0400

    allo

--- app/src/main/java/com/example/convertisseur/MainActivity.java
+++ app/src/main/java/com/example/convertisseur/MainActivity.java
@@ -3,10 +3,22 @@ package com.example.convertisseur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+   EditText editText;
+    public void convert(View view) {
+        Double canadianAmount, americanAmount;
+        editText=findViewById(R.id.editText);
+        canadianAmount = Double.parseDouble(editText.getText().toString());
+        americanAmount = canadianAmount*0.76;
+        Toast.makeText(MainActivity.this,String.format("%.2f",americanAmount) + " $ US ", Toast.LENGTH_LONG).show();
 
-    @Override
+    }
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);

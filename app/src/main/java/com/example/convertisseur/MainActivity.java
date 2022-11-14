b556c8337aabf2c8082b32d0c1698c5d9c68ac44
botato2048 <ema@tfs.ca>
2022-11-14 08:51:53 -0500

    hide input window

--- app/src/main/java/com/example/convertisseur/MainActivity.java
+++ app/src/main/java/com/example/convertisseur/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.convertisseur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,8 +16,12 @@ public class MainActivity extends AppCompatActivity {
         editText=findViewById(R.id.editText);
         canadianAmount = Double.parseDouble(editText.getText().toString());
         americanAmount = canadianAmount*0.76;
+
+        InputMethodManager mgr=(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
         Toast.makeText(MainActivity.this,String.format("%.2f",americanAmount) + " $ US ", Toast.LENGTH_LONG).show();
-        //HIIIII
+
     }
 
         @Override
